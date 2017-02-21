7f46dafd054659c54a1f7fa3cc8688ffd63fd760
Heather Shapiro <hshapiro93@gmail.com>
2017-02-21 11:37:14 -0500

    updated restify

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    
+    // appId: process.env.MICROSOFT_APP_ID,
+    appId: 'b71b29d2-bcba-467c-a4f9-f1e2cbbe61e8',
+    appPassword: 'KbCjMJkhzW0c5nSSaVC5ShT'
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
