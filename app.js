18993ec9d619fe1545923df2606cb7d325f77a6d
Heather Shapiro <hshapiro93@gmail.com>
2017-02-21 13:13:07 -0500

    updated greeting

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ bot.recognizer(new builder.LuisRecognizer('https://westus.api.cognitive.microsof
 
 var companyData = require('./companyData.json');
 
-bot.dialog('start',function (session) {
-        session.send("Hi, I'm Amy!")
-        setTimeout(function(){
-            session.beginDialog('helpDialog');
-        },1000);  
-    }).triggerAction({matches: /^hi/i});
+// bot.dialog('start',function (session) {
+//         session.send("Hi, I'm Amy!")
+//         setTimeout(function(){
+//             session.beginDialog('helpDialog');
+//         },1000);  
+//     }).triggerAction({matches: /^hi/i});
 
 bot.dialog('helpDialog', function (session) {
     // Send help message and end dialog.
